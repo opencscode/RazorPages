3fb0d3e99b14846f727d9ca440d1d871daba305a
Umer Hasan <cscexcellence@gmail.com>
2017-12-18 23:03:19 +0500

    Nightly Build

--- RazorPages/Startup.cs
+++ RazorPages/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Models;
 
 namespace RazorPages
 {
@@ -21,6 +23,7 @@ namespace RazorPages
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MovieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
             services.AddMvc();
         }
 
